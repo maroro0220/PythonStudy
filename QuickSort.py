ae0a092f69132b6279289eb0c3699b8cbc3eb8ce
maroro0220 <maro1339@gmail.com>
2020-04-05 21:51:53 +0900

    practice Quick Sort

--- QuickSort.py
+++ QuickSort.py
@@ -1,27 +1,30 @@
 def Quick(arr,l,r):
     if l>r:
-        return
-    pivo=(l+r)//2
+        return arr
+    pivo=arr[(l+r)//2]
     #pivo=l
     left=l
     right=r
     
     while(left<=right):
-        while(arr[left]<arr[pivo] ):
+        while(arr[left]<pivo ):
             left+=1
-        while(arr[pivo]<arr[right] ):
+        while( pivo<arr[right] ):
             right-=1
-        tmp=arr[left]
-        arr[left]=arr[right]
-        arr[right]=tmp
-        left+=1
-        right-=1
+        if(left<=right):
+       # if True:
+            tmp=arr[left]
+            arr[left]=arr[right]
+            arr[right]=tmp
+            left+=1
+            right-=1
     #if(left<right):
-    #    tmp=arr[pivo]
-    #    arr[pivo]=arr[right]
-    #    arr[right]=tmp
-    Quick(arr,l,right)
-    Quick(arr,left,r)
+     #   tmp=arr[pivo]
+     #   arr[pivo]=arr[right]
+     #   arr[right]=tmp
+    #if True:
+        Quick(arr,l,right)
+        Quick(arr,left,r)
     return arr
-array=[5,4,2,3,4,1]
+array=[5,4,6,3,4,1]
 print(Quick(array,0,len(array)-1))
