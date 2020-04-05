19ea4525cff41439703713d27e762c92dc329b2e
maroro0220 <maro1339@gmail.com>
2020-04-05 16:58:19 +0900

    Using buble sort for practice

--- Programmers/Programmers_KthNumber.py
+++ Programmers/Programmers_KthNumber.py
@@ -1,10 +1,10 @@
 def Sort(arr):
-    for i in range(len(arr)-1):
-        for j in range(i+1,len(arr)):
-            if(arr[j]<arr[i]):
+    for i in range(len(arr)-1,0,-1):
+        for j in range(0,i):
+            if(arr[j+1]<arr[j]):
                 tmp=arr[j]
-                arr[j]=arr[i]
-                arr[i]=tmp
+                arr[j]=arr[j+1]
+                arr[j+1]=tmp
     return arr
 
 def solution(array, commands):
