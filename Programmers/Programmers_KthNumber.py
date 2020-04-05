83b754b44d962866db29a330116eaf4a2d437923
maroro0220 <maro1339@gmail.com>
2020-04-05 16:41:38 +0900

    Using buble sort for practice

--- Programmers/Programmers_KthNumber.py
+++ Programmers/Programmers_KthNumber.py
@@ -1,8 +1,17 @@
+def Sort(arr):
+    for i in range(len(arr)-1):
+        for j in range(i+1,len(arr)):
+            if(arr[j]<arr[i]):
+                tmp=arr[j]
+                arr[j]=arr[i]
+                arr[i]=tmp
+    return arr
+
 def solution(array, commands):
     answer = []
     for i in commands:
         slic=array[i[0]-1:i[1]]
-        slic.sort()
+        slic=Sort(slic)
         answer.append(slic[i[2]-1])
     
     return answer
