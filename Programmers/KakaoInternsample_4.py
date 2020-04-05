03f97eeb75cbf232a2779110cd87e7ed6f36816e
maroro0220 <maro1339@gmail.com>
2020-04-05 16:40:50 +0900

    Kakao Internship2019 winter test 4th Problem. Using dict

--- Programmers/KakaoInternsample_4.py
+++ Programmers/KakaoInternsample_4.py
@@ -2,15 +2,21 @@ def solution(k, room_number):
     answer = []
     m=dict()
     for i in room_number:
-        if(not i in m):
+        if( i not in m.keys()):
             answer.append(i)
-            m[i]=True
+            m[i]=i+1;
+             
         else:
-            for j in range(i+1,k):
-                if(not j in m):
-                    answer.append(j)
-                    m[j]=True
-                    break
+            b=[]
+            j=m[i]
+            while j in m:
+                b.append(j)
+                j=m[j]
+            for bb in b:# This is important. if input is [1,1,1,3,1,1,1], result after this function is m[3] = 6. So it sholud be update 
+                m[bb]=j+1
+            m[j]=j+1
+            answer.append(j)
             
     return answer
 print(solution(10, [1,3,4,1,3,1]))
+#print(solution(10, [1, 2, 3, 1, 1]))
